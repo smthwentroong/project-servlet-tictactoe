051ddb9260e12ab3141f0f6e975cd6d6f59c8961
kkopienko <kirill.kopienko@gmail.com>
2023-11-26 03:50:10 +0300

    table requests

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,26 @@
+package com.tictactoe;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+@WebServlet(value = "/logic")
+public class LogicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int index = getSelectedIndex(req);
+        resp.sendRedirect("/index.jsp");
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+}
